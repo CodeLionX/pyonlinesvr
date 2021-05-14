8666824379fbc9516e3d544be8d7af4887bd2839
CodeLionX <sebastian.schmidl@t-online.de>
2021-05-14 10:40:56 +0200

    feat: add internal conversion functions for IntMatrix and IntVector

--- pyonlinesvr/lib/compat.py
+++ pyonlinesvr/lib/compat.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List
 
-from pyonlinesvr.lib.onlinesvr import OnlineSVR, DoubleMatrix, DoubleVector
+from pyonlinesvr.lib.onlinesvr import IntVector, IntMatrix, OnlineSVR, DoubleMatrix, DoubleVector
 from sklearn.utils.validation import check_array
 import numpy as np
 
@@ -32,7 +32,7 @@ def np_to_double_vector(arr: np.ndarray) -> DoubleVector:
 
 def double_vector_to_np(vec: DoubleVector) -> np.ndarray:
     length = vec.GetLength()
-    arr = np.empty(length)
+    arr = np.empty(length, dtype=np.float64, order="C")
     for i in range(length):
         arr[i] = vec.GetValue(i)
     return arr
@@ -42,7 +42,7 @@ def np_to_double_matrix(arr: np.ndarray) -> DoubleMatrix:
     arr = check_array(arr, dtype=np.float64, ensure_2d=True, order="C", copy=False,
                       accept_sparse=False, accept_large_sparse=False)
     m = DoubleMatrix()
-    for i, row in enumerate(arr):
+    for row in arr:
         row_vec = np_to_double_vector(row)
         m.AddRowCopy(row_vec)
     return m
@@ -52,7 +52,49 @@ def double_matrix_to_np(m: DoubleMatrix) -> np.ndarray:
     rows = m.GetLengthRows()
     cols = m.GetLengthCols()
 
-    arr = np.empty((rows, cols))
+    arr = np.empty((rows, cols), dtype=np.float64, order="C")
+    for i in range(rows):
+        for j in range(cols):
+            arr[i, j] = m.GetValue(i, j)
+    return arr
+
+
+def np_to_int_vector(arr: np.ndarray) -> IntVector:
+    arr = check_array(arr, dtype=np.int8, ensure_2d=False, order="C", copy=False,
+                      accept_sparse=False, accept_large_sparse=False)
+    if len(arr.shape) > 1:
+        raise ValueError("Can only convert a 1-dimensional array into a IntVector! "
+                         f"Input array has shape={arr.shape}")
+
+    vec = IntVector()
+    for v in arr:
+        vec.Add(v)
+    return vec
+
+
+def int_vector_to_np(vec: IntVector) -> np.ndarray:
+    length = vec.GetLength()
+    arr = np.empty(length, dtype=np.int8, order="C")
+    for i in range(length):
+        arr[i] = vec.GetValue(i)
+    return arr
+
+
+def np_to_int_matrix(arr: np.ndarray) -> IntMatrix:
+    arr = check_array(arr, dtype=np.int8, ensure_2d=True, order="C", copy=False,
+                      accept_sparse=False, accept_large_sparse=False)
+    m = IntMatrix()
+    for row in arr:
+        row_vec = np_to_int_vector(row)
+        m.AddRowCopy(row_vec)
+    return m
+
+
+def int_matrix_to_np(m: IntMatrix) -> np.ndarray:
+    rows = m.GetLengthRows()
+    cols = m.GetLengthCols()
+
+    arr = np.empty((rows, cols), dtype=np.int8, order="C")
     for i in range(rows):
         for j in range(cols):
             arr[i, j] = m.GetValue(i, j)
